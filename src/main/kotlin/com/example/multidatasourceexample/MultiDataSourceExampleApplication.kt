dff8c0b60f29359b7d90a5003fe03077d717931d
yxxnghwan <younghwan960@gmail.com>
2023-11-27 22:39:16 +0900

    project init

---
+++ src/main/kotlin/com/example/multidatasourceexample/MultiDataSourceExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.multidatasourceexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultiDataSourceExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<MultiDataSourceExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/multidatasourceexample/MultiDataSourceExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.multidatasourceexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiDataSourceExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
