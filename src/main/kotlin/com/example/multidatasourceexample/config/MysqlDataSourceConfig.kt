bfbb2ee3499857f061363f0f4296d2184f8644e7
yxxnghwan <younghwan960@gmail.com>
2023-12-07 01:44:03 +0900

    feat: 레플리케이션 멀티 데이터 소스 설정 및 하이버네이트 설정 받도록 fix

--- src/main/kotlin/com/example/multidatasourceexample/config/MysqlDataSourceConfig.kt
+++ src/main/kotlin/com/example/multidatasourceexample/config/MysqlDataSourceConfig.kt
@@ -1,20 +1,33 @@
 package com.example.multidatasourceexample.config
 
+import com.zaxxer.hikari.HikariDataSource
 import jakarta.persistence.EntityManagerFactory
+import org.hibernate.cfg.AvailableSettings
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
 import org.springframework.context.annotation.Primary
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
+import org.springframework.orm.hibernate5.SpringBeanContainer
 import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.support.TransactionSynchronizationManager
 import javax.sql.DataSource
 
 
@@ -22,45 +35,94 @@ import javax.sql.DataSource
 @EntityScan(basePackages = ["com.example.multidatasourceexample.domain.mysql"])
 @EnableJpaRepositories(
     basePackages = ["com.example.multidatasourceexample.domain.mysql"],
-    entityManagerFactoryRef = "mysqlEntityManagerFactory",
-    transactionManagerRef = "mysqlTransactionManager"
+    entityManagerFactoryRef = MysqlDataSourceConfig.DATABASE_NAME + "EntityManagerFactory",
+    transactionManagerRef = MysqlDataSourceConfig.DATABASE_NAME + "TransactionManager"
 )
-class MysqlDataSourceConfig {
+class MysqlDataSourceConfig(
+    private val jpaProperties: JpaProperties,
+    private val hibernateProperties: HibernateProperties,
+) {
+
+    companion object {
+        const val DATABASE_NAME = "mysql"
+    }
+
+    @Value("\${spring.datasource.mysql.dialect}")
+    private val dialect: String? = null
 
     @Primary
-    @Bean("mysqlDataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource.mysql")
-    fun mysqlDataSourceProperties(): DataSourceProperties {
-        return DataSourceProperties()
+    @DependsOn(DATABASE_NAME + "WriteDataSource", DATABASE_NAME + "ReadonlyDataSource")
+    @Bean(name = [DATABASE_NAME + "DataSource"])
+    fun dataSource(
+        @Qualifier(DATABASE_NAME + "WriteDataSource") writeDataSource: DataSource,
+        @Qualifier(DATABASE_NAME + "ReadonlyDataSource") readonlyDataSource: DataSource
+    ): DataSource {
+        val routingDataSource = ReplicationRoutingDataSource()
+        val dataSourceMap: MutableMap<Any, Any> = HashMap()
+        dataSourceMap["write"] = writeDataSource
+        dataSourceMap["read"] = readonlyDataSource
+        routingDataSource.setTargetDataSources(dataSourceMap)
+        routingDataSource.setDefaultTargetDataSource(dataSourceMap["write"]!!)
+        routingDataSource.afterPropertiesSet()
+        return LazyConnectionDataSourceProxy(routingDataSource)
+    }
+
+    private class ReplicationRoutingDataSource : AbstractRoutingDataSource() {
+        override fun determineCurrentLookupKey(): Any {
+            return if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) "read" else "write"
+        }
+    }
+
+    @Bean(name = [DATABASE_NAME + "WriteDataSource"])
+    @ConfigurationProperties(prefix = "spring.datasource.mysql.main")
+    fun writeDataSource(): DataSource {
+        return HikariDataSource()
+    }
+
+    @Bean(name = [DATABASE_NAME + "ReadonlyDataSource"])
+    @ConfigurationProperties(prefix = "spring.datasource.mysql.readonly")
+    fun readonlyDataSource(): DataSource {
+        return HikariDataSource()
     }
 
     @Primary
-    @Bean("mysqlDataSource")
-    fun dataSource(): DataSource {
-        return mysqlDataSourceProperties().initializeDataSourceBuilder().build()
+    @Bean(name = [DATABASE_NAME + "JdbcTemplate"])
+    fun jdbcTemplate(@Qualifier(DATABASE_NAME + "WriteDataSource") dataSource: DataSource): JdbcTemplate {
+        return JdbcTemplate(dataSource)
     }
 
     @Primary
-    @Bean("mysqlEntityManagerFactory")
+    @Bean(name = [DATABASE_NAME + "EntityManagerFactory"])
     fun entityManagerFactory(
-        @Qualifier("mysqlDataSource") dataSource: DataSource,
+        @Qualifier(DATABASE_NAME + "DataSource") dataSource: DataSource,
         builder: EntityManagerFactoryBuilder,
+        beanFactory: ConfigurableListableBeanFactory
     ): LocalContainerEntityManagerFactoryBean {
-        return builder
-            .dataSource(dataSource)
-            .packages("com.example.multidatasourceexample.domain.mysql") // MySQL 엔터티 패키지
-            .persistenceUnit("mysql")
-            .properties(
-                mapOf(Pair("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect"))
-            )
+        val emfb = builder.dataSource(dataSource)
+            .persistenceUnit(DATABASE_NAME)
+            .properties(jpaProperties())
+            .packages("com.example.multidatasourceexample.domain.mysql")
             .build()
+        emfb.jpaPropertyMap[AvailableSettings.BEAN_CONTAINER] = SpringBeanContainer(beanFactory)
+        return emfb
     }
 
     @Primary
-    @Bean("mysqlTransactionManager")
+    @Bean(name = [DATABASE_NAME + "TransactionManager"])
     fun transactionManager(
-        @Qualifier("mysqlEntityManagerFactory") entityManagerFactory: LocalContainerEntityManagerFactoryBean,
+        @Qualifier(DATABASE_NAME + "EntityManagerFactory") entityManagerFactory: EntityManagerFactory
     ): PlatformTransactionManager {
-        return JpaTransactionManager(entityManagerFactory.`object`!!)
+        val transactionManager = JpaTransactionManager()
+        transactionManager.entityManagerFactory = entityManagerFactory
+        return transactionManager
+    }
+
+    private fun jpaProperties(): Map<String, Any?>? {
+        val properties = hibernateProperties.determineHibernateProperties(
+            jpaProperties.properties, HibernateSettings()
+        )
+        properties["hibernate.dialect"] = dialect
+        properties["javax.persistence.lock.timeout"] = 0
+        return properties
     }
 }
--- src/main/kotlin/com/example/multidatasourceexample/config/PostgresqlDataSourceConfig.kt
+++ src/main/kotlin/com/example/multidatasourceexample/config/PostgresqlDataSourceConfig.kt
@@ -1,19 +1,33 @@
 package com.example.multidatasourceexample.config
 
+import com.zaxxer.hikari.HikariDataSource
 import jakarta.persistence.EntityManagerFactory
+import org.hibernate.cfg.AvailableSettings
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
+import org.springframework.context.annotation.Primary
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
+import org.springframework.orm.hibernate5.SpringBeanContainer
 import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.support.TransactionSynchronizationManager
 import javax.sql.DataSource
 
 
@@ -21,41 +35,90 @@ import javax.sql.DataSource
 @EntityScan(basePackages = ["com.example.multidatasourceexample.domain.postgresql"])
 @EnableJpaRepositories(
     basePackages = ["com.example.multidatasourceexample.domain.postgresql"],
-    entityManagerFactoryRef = "postgresqlEntityManagerFactory",
-    transactionManagerRef = "postgresqlTransactionManager"
+    entityManagerFactoryRef = PostgresqlDataSourceConfig.DATABASE_NAME + "EntityManagerFactory",
+    transactionManagerRef = PostgresqlDataSourceConfig.DATABASE_NAME + "TransactionManager"
 )
-class PostgresqlDataSourceConfig {
+class PostgresqlDataSourceConfig(
+    private val jpaProperties: JpaProperties,
+    private val hibernateProperties: HibernateProperties,
+) {
 
-    @Bean("postgresqlDataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource.postgresql")
-    fun postgresqlDataSourceProperties(): DataSourceProperties {
-        return DataSourceProperties()
+    companion object {
+        const val DATABASE_NAME = "postgresql"
     }
 
-    @Bean("postgresqlDataSource")
-    fun postgresqlDataSource(): DataSource {
-        return postgresqlDataSourceProperties().initializeDataSourceBuilder().build()
+    @Value("\${spring.datasource.postgresql.dialect}")
+    private val dialect: String? = null
+
+    @DependsOn(DATABASE_NAME + "WriteDataSource", DATABASE_NAME + "ReadonlyDataSource")
+    @Bean(name = [DATABASE_NAME + "DataSource"])
+    fun dataSource(
+        @Qualifier(DATABASE_NAME + "WriteDataSource") writeDataSource: DataSource,
+        @Qualifier(DATABASE_NAME + "ReadonlyDataSource") readonlyDataSource: DataSource
+    ): DataSource {
+        val routingDataSource = ReplicationRoutingDataSource()
+        val dataSourceMap: MutableMap<Any, Any> = HashMap()
+        dataSourceMap["write"] = writeDataSource
+        dataSourceMap["read"] = readonlyDataSource
+        routingDataSource.setTargetDataSources(dataSourceMap)
+        routingDataSource.setDefaultTargetDataSource(dataSourceMap["write"]!!)
+        routingDataSource.afterPropertiesSet()
+        return LazyConnectionDataSourceProxy(routingDataSource)
     }
 
-    @Bean("postgresqlEntityManagerFactory")
-    fun postgresqlEntityManagerFactory(
-        @Qualifier("postgresqlDataSource") dataSource: DataSource,
+    private class ReplicationRoutingDataSource : AbstractRoutingDataSource() {
+        override fun determineCurrentLookupKey(): Any {
+            return if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) "read" else "write"
+        }
+    }
+
+    @Bean(name = [DATABASE_NAME + "WriteDataSource"])
+    @ConfigurationProperties(prefix = "spring.datasource.postgresql.main")
+    fun writeDataSource(): DataSource {
+        return HikariDataSource()
+    }
+
+    @Bean(name = [DATABASE_NAME + "ReadonlyDataSource"])
+    @ConfigurationProperties(prefix = "spring.datasource.postgresql.readonly")
+    fun readonlyDataSource(): DataSource {
+        return HikariDataSource()
+    }
+
+    @Bean(name = [DATABASE_NAME + "JdbcTemplate"])
+    fun jdbcTemplate(@Qualifier(DATABASE_NAME + "WriteDataSource") dataSource: DataSource): JdbcTemplate {
+        return JdbcTemplate(dataSource)
+    }
+
+    @Bean(name = [DATABASE_NAME + "EntityManagerFactory"])
+    fun entityManagerFactory(
+        @Qualifier(DATABASE_NAME + "DataSource") dataSource: DataSource,
         builder: EntityManagerFactoryBuilder,
+        beanFactory: ConfigurableListableBeanFactory
     ): LocalContainerEntityManagerFactoryBean {
-        return builder
-            .dataSource(dataSource)
-            .packages("com.example.multidatasourceexample.domain.postgresql") // PostgreSQL 엔터티 패키지
-            .persistenceUnit("postgresql")
-            .properties(
-                mapOf(Pair("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect"))
-            )
+        val emfb = builder.dataSource(dataSource)
+            .persistenceUnit(DATABASE_NAME)
+            .properties(jpaProperties())
+            .packages("com.example.multidatasourceexample.domain.postgresql")
             .build()
+        emfb.jpaPropertyMap[AvailableSettings.BEAN_CONTAINER] = SpringBeanContainer(beanFactory)
+        return emfb
     }
 
-    @Bean("postgresqlTransactionManager")
-    fun postgresqlTransactionManager(
-        @Qualifier("postgresqlEntityManagerFactory") entityManagerFactory: LocalContainerEntityManagerFactoryBean,
+    @Bean(name = [DATABASE_NAME + "TransactionManager"])
+    fun transactionManager(
+        @Qualifier(DATABASE_NAME + "EntityManagerFactory") entityManagerFactory: EntityManagerFactory
     ): PlatformTransactionManager {
-        return JpaTransactionManager(entityManagerFactory.`object`!!)
+        val transactionManager = JpaTransactionManager()
+        transactionManager.entityManagerFactory = entityManagerFactory
+        return transactionManager
+    }
+
+    private fun jpaProperties(): Map<String, Any?>? {
+        val properties = hibernateProperties.determineHibernateProperties(
+            jpaProperties.properties, HibernateSettings()
+        )
+        properties["hibernate.dialect"] = dialect
+        properties["javax.persistence.lock.timeout"] = 0
+        return properties
     }
 }
